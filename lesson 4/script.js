3a56b5c2dd60d63d7c67ae4181f3228a71d6bee7
HayriyanHarut <harut.hayriyan.s@tumo.org>
2019-10-15 20:36:45 +0400

    Helo world

--- lesson 4/script.js
+++ lesson 4/script.js
@@ -1,24 +1,48 @@
-function main() {
-    var socket = io();
-    var chatDiv = document.getElementById('chat');
-    var input = document.getElementById('message');
-    var button = document.getElementById('submit');
+// function main() {
+//     var socket = io();
+//     var chatDiv = document.getElementById('chat');
+//     var input = document.getElementById('message');
+//     var button = document.getElementById('submit');
 
-    function handleSubmit(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("send message", val);
-        }
-    }
-    button.onclick = handleSubmit;
-    function handleMessage(msg) {
-        var p = document.createElement('p');
-        p.innerText = msg;
-        chatDiv.appendChild(p);
-        input.value = "";
-    }
+//     function handleSubmit(evt) {
+//         var val = input.value;
+//         if (val != "") {
+//             socket.emit("send message", val);
+//         }
+//     }
+//     button.onclick = handleSubmit;
+//     function handleMessage(msg) {
+//         var p = document.createElement('p');
+//         p.innerText = msg;
+//         chatDiv.appendChild(p);
+//         input.value = "";
+//     } 
 
-    socket.on('display message', handleMessage);
+
+//     socket.on('display message', handleMessage);
+// }
+
+// window.onload = main; 
+
+function setup() {
+    createCanvas(1000, 700);
+    background(230);
+    strokeWeight(2);
 }
 
-window.onload = main; 
+let value = 0;
+function draw() {
+    fill("blue")
+   
+}
+function mouseDragged() {
+    value = value + 5;
+    if (value > 255) {
+        value = 0;
+    }
+}
+ function mouseDragged() {
+    ellipse(mouseX, mouseY, 50, 50);
+    return false;
+    
+}
